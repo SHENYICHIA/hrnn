50c7df2f9d9a626749d0bf28cdbf35b3bf7505fe
SHENTICHIA <d1320262@o365.fcu.edu.tw>
2024-09-23 16:56:42 +0800

    add M1-Q04.py file

---
+++ M1-Q04.py
@@ -0,0 +1,5 @@
+c,e,m=map(int,input().split())
+total=c+e+m
+average=int(total/3)
+print(total)
+print(average)
---
+++ eee.py
@@ -0,0 +1,14 @@
+a=255
+print(f'{a:05d}')  # f = 格式化輸出 # 冒號後面是格式
+
+print(f'hello',end='')
+print(f'world')
+
+print(f'hello',end='#')
+print(f'world')
+
+# \n = 換行
+
+b=3.1415926
+print(f'{b:.2f}') #小數點後的浮點數
+
